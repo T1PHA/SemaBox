9623db76c8e070b2cd0c1f494bbe15d9038e10b7
T1PHA <tiphaelflak@gmail.com>
2023-02-23 21:52:33 +0100

    Modifs tdb avec speedtest et machines dans reseau

--- scan.py
+++ scan.py
@@ -1,18 +1,82 @@
+import socket
+import threading
 import tkinter as tk
-from scapy.all import *
 
-def get_machines():
-    machines = []
-    ans, unans = arping('10.60.56.0/24', verbose=0)
-    for s, r in ans:
+class PortScanner:
+    root = tk.Tk()
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Port Scanner")
+
+        # Ajout d'un champ d'entrée pour l'adresse IP
+        self.ip_label = tk.Label(self.master, text="IP Address:")
+        self.ip_label.pack()
+        self.ip_entry = tk.Entry(self.master)
+        self.ip_entry.pack()
+
+        # Ajout d'un champ d'entrée pour la plage de ports à scanner
+        self.port_range_label = tk.Label(self.master, text="Port Range:")
+        self.port_range_label.pack()
+        self.port_range_entry = tk.Entry(self.master)
+        self.port_range_entry.insert(tk.END, "1-1000")
+        self.port_range_entry.pack()
+
+        # Ajout d'un bouton pour lancer le scan
+        self.scan_button = tk.Button(self.master, text="Scan", command=self.scan_ports)
+        self.scan_button.pack()
+
+        # Ajout d'un champ de texte pour afficher les résultats
+        self.result_text = tk.Text(self.master)
+        self.result_text.pack()
+
+    def scan_ports(self):
+        # Récupération de l'adresse IP entrée
+        ip = self.ip_entry.get()
+
+        # Récupération de la plage de ports à scanner
+        port_range = self.port_range_entry.get().split('-')
+        start_port = int(port_range[0])
+        end_port = int(port_range[1])
+
+        # Scan des ports pour chaque machine du réseau
+        for port in range(start_port, end_port+1):
+            # Utilisation de threads pour accélérer le scan
+            thread = threading.Thread(target=self.scan_port, args=(ip, port))
+            thread.start()
+            
+    host = socket.gethostname()
+    
+    # obtenir la liste des noms de machines dans le réseau local
+    ip_list = []
+    for ip in socket.gethostbyname_ex(host)[2]:
+        if not ip.startswith("127."):
+            ip_list.append(ip)
+
+    # créer une chaîne de caractères avec les noms de machines séparés par des sauts de ligne
+    machines_str = "\n".join(ip_list)
+
+    # afficher la liste des machines dans un label
+    label = tk.Label(root, text=f'Machines détectées dans le réseau:\n {machines_str}')
+    label.pack()
+
+    root.mainloop()
+
+    def scan_port(self, ip, port):
+        # Tentative de connexion à un port sur une machine
         try:
-            name = socket.gethostbyaddr(r.psrc)[0]
-            machines.append(f'{r.sprintf(r"%Ether.src%")} - {r.sprintf(r"%ARP.psrc%")} - {name}')
-        except:
-            machines.append(f'{r.sprintf(r"%Ether.src%")} - {r.sprintf(r"%ARP.psrc%")}')
-    return '\n'.join(machines)
+            # Création d'un objet socket
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(1)
+            result = sock.connect_ex((ip, port))
+            sock.close()
 
+            # Si la connexion est réussie, le port est ouvert
+            if result == 0:
+                self.result_text.insert(tk.END, f"Port {port} is open\n")
+
+        except:
+            pass
+            
 root = tk.Tk()
-label = tk.Label(root, text=get_machines())
-label.pack()
+scanner = PortScanner(root)
 root.mainloop()
--- tdb.py
+++ tdb.py
@@ -3,8 +3,8 @@ import tkinter as tk
 import requests
 import subprocess
 from scapy.all import *
-
-
+import urllib.request
+import nmap
 
 def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -30,12 +30,8 @@ def get_dynamic_dns():
     return response.text.split('Dynamic DNS : ')[-1].split('<')[0]
 
 #scan réseau
-def get_machines():
-    machines = []
-    ans, unans = arping('10.60.56.0/24')
-    for s, r in ans:
-        machines.append(r.sprintf("%ARP.psrc%"))
-    return "\n".join(machines)
+# appel de la fonction pour afficher la liste des noms de machines
+
 
 root = tk.Tk()
 label_titre = tk.Label(root, text=f'Bienvenue sur le tableau de bord de votre SemaOS')
@@ -44,38 +40,83 @@ label_titre.pack(side="top")
 # Fonction qui met à jour le label avec la latence de ping
 def check_internet_connection():
     try:
-        result = subprocess.run(['ping', '-c', '1', '8.8.8.8'], stdout=subprocess.PIPE)
-        
-        if result.returncode == 0:
-            latency = result.stdout.decode('latin1').split('\n')[-2].split(' = ')[-1]
-            label_latency['text'] = f'Connecté à Internet\nLatency: {latency}'
-        else:
-            label_latency['text'] = 'Non connecté à Internet'
-    except Exception as e:
-        label_latency['text'] = 'Error: ' + str(e)
+        # essayer de se connecter à un site web connu
+        urllib.request.urlopen("http://www.google.com")
+        label = tk.Label(root, text="Votre machine est connectée à Internet.")
+    except:
+        label = tk.Label(root, text="Votre machine n'est pas connectée à Internet.")
+    label.pack()
+
+# appel de la fonction pour vérifier la connexion Internet
+
 
 # texte = tk.Text(root, height=10)
 # texte.pack()
 # texte.configure(font=("Times New Roman", 20, "italic"))
 
-#scan label
-label_machines = tk.Label(root, text=f'Machines en ligne: {get_machines()}')
-label_machines.pack(side="bottom")
+
+# ajouter d'autres fonctionnalités à votre tableau de bord ici
+
+# lancement de la boucle principale Tkinter
 
 label_ip = tk.Label(root, text=f'IP: {get_ip()}')
 label_ip.pack()
 
 label_hostname = tk.Label(root, text=f'Hostname: {get_hostname()}')
 label_hostname.pack()
-
+check_internet_connection()
 label_latency = tk.Label(root, text='')
 label_latency.pack()
-check_internet_connection()
 
 label_ip = tk.Label(root, text=f'Public IP: {get_public_ip()}')
 label_ip.pack()
 label_dns = tk.Label(root, text=f'Dynamic DNS: {get_dynamic_dns()}')
 label_dns.pack()
+
+url = "http://speedtest.ftp.otenet.gr/files/test100k.db"
+
+# Taille du fichier en octets
+size_in_bytes = 102400
+
+# Fonction appelée lorsque le bouton "Lancer le speedtest" est cliqué
+def start_speedtest():
+  # Mesure du temps de début du téléchargement
+  start = time.perf_counter()
+
+  # Téléchargement du fichier
+  urllib.request.urlretrieve(url, "file.bin")
+
+  # Mesure du temps de fin du téléchargement
+  end = time.perf_counter()
+
+  # Calcul de la durée du téléchargement en secondes
+  duration = end - start
+
+  # Calcul de la vitesse de téléchargement en octets par seconde
+  speed = size_in_bytes / duration
+
+  # Affichage de la vitesse de téléchargement en Mo/s dans une nouvelle fenêtre
+
+  result_label = tk.Label(root, text=f"Vitesse de téléchargement : {speed / 1024 / 1024:.2f} Mo/s")
+  result_label.pack()
+
+start_speedtest()
+
+#scan label
+host = socket.gethostname()
+
+# obtenir la liste des noms de machines dans le réseau local
+ip_list = []
+for ip in socket.gethostbyname_ex(host)[2]:
+    if not ip.startswith("127."):
+        ip_list.append(ip)
+
+# créer une chaîne de caractères avec les noms de machines séparés par des sauts de ligne
+machines_str = ip_list
+
+# afficher la liste des machines dans un label
+label = tk.Label(root, text=f'Machines détectées dans le réseau: {machines_str}')
+label.pack()
 
 root.geometry("400x300")
 root.mainloop()
